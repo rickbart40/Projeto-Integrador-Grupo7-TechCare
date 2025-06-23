8d74bd886117d09b329e43c65ffa00a21d63b2d4
RickSander40 <ricksander40@gmail.com>
2025-06-23 19:48:58 -0300

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,17 @@
+var radio = document.querySelectorAll('.manu-btn');
+var cont = 1;
+
+document.getElementById('radio1').checked = true;
+
+setInterval(()=> {
+    proximaImg()
+},3000);  
+
+function proximaImg() {
+    cont++;
+    if(cont > 4) {
+        cont = 1;
+    }
+
+    document.getElementById('radio' + cont).checked = true;
+}
